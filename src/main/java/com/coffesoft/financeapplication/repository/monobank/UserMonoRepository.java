a46b0ba9fa212c4078b35fc671f6569612259257
Maxuha <zykovmaksym@gmail.com>
2020-08-11 10:28:15 +0300

    Change type id

--- src/main/java/com/coffesoft/financeapplication/repository/monobank/UserMonoRepository.java
+++ src/main/java/com/coffesoft/financeapplication/repository/monobank/UserMonoRepository.java
@@ -3,5 +3,5 @@ package com.coffesoft.financeapplication.repository.monobank;
 import com.coffesoft.financeapplication.model.monobank.UserMono;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserMonoRepository extends JpaRepository<UserMono, Long> {
+public interface UserMonoRepository extends JpaRepository<UserMono, String> {
 }
