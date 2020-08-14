0f56ab5e8415390b43d81401f11b4b56a02174f5
Maxuha <zykovmaksym@gmail.com>
2020-08-14 15:24:07 +0300

    Added crud operation

--- src/main/java/com/coffesoft/financeapplication/service/system/WalletService.java
+++ src/main/java/com/coffesoft/financeapplication/service/system/WalletService.java
@@ -1,5 +1,6 @@
 package com.coffesoft.financeapplication.service.system;
 
+import com.coffesoft.financeapplication.exception.NotFoundException;
 import com.coffesoft.financeapplication.exception.WalletNotFoundException;
 import com.coffesoft.financeapplication.model.system.Wallet;
 
@@ -8,6 +9,7 @@ import java.util.List;
 public interface WalletService {
     List<Wallet> findAllWallet();
     Wallet findByIdWallet(Long id) throws WalletNotFoundException;
+    Wallet findByUserIdWallet(Long userId) throws NotFoundException;
     Wallet saveWallet(Wallet wallet);
     Wallet updateWallet(Wallet wallet) throws WalletNotFoundException;
     void deleteWallet(Wallet wallet) throws WalletNotFoundException;
