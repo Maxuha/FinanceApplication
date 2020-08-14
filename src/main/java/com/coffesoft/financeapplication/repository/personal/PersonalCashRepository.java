0c22b7fcf4c6fdfc34d7f23cdc4d2e0a3a12daa2
Maxuha <zykovmaksym@gmail.com>
2020-08-14 18:13:55 +0300

    find personal cash by wallet

--- src/main/java/com/coffesoft/financeapplication/repository/personal/PersonalCashRepository.java
+++ src/main/java/com/coffesoft/financeapplication/repository/personal/PersonalCashRepository.java
@@ -3,5 +3,8 @@ package com.coffesoft.financeapplication.repository.personal;
 import com.coffesoft.financeapplication.model.personal.PersonalCash;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PersonalCashRepository extends JpaRepository<PersonalCash, Long> {
+    Optional<PersonalCash> findByWalletId(Long walletId);
 }
