f4add33966f8d764ae4433d933fdabeb4206d889
Maxuha <zykovmaksym@gmail.com>
2020-08-14 21:11:06 +0300

    Added util class for get request

---
+++ src/main/java/com/coffesoft/financeapplication/util/GetRequest.java
@@ -0,0 +1,8 @@
+package com.coffesoft.financeapplication.util;
+
+import java.util.Map;
+
+public interface GetRequest {
+    String getRequest(String url);
+    String getWithHeaderRequest(String url, Map<String, String> headers);
+}
